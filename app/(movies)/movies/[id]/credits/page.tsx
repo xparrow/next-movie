bc213b90a71d0befe21d8f79eb570ddd54703657
darkwhat@naver.com <darkwhat@naver.com>
2024-02-16 09:57:22 +0900

    modified:   app/(movies)/movies/[id]/credits/page.tsx modified:   app/(movies)/movies/[id]/providers/page.tsx modified:   app/(movies)/movies/[id]/similar/page.tsx

--- app/(movies)/movies/[id]/credits/page.tsx
+++ app/(movies)/movies/[id]/credits/page.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from "react";
 import MovieCredits from "../../../../../components/movie-credits";
 import type { IParms } from "../layout";
+import Loading from "../../../../../components/Loading";
 
 export default function Credits({ params:{id}}:IParms){
   return(
-    <Suspense>
+    <Suspense fallback={<Loading />}>
       <MovieCredits id={id} />
     </Suspense>
   );
--- app/(movies)/movies/[id]/similar/page.tsx
+++ app/(movies)/movies/[id]/similar/page.tsx
@@ -1,8 +1,12 @@
+import { Suspense } from "react";
 import MovieSimilar from "../../../../../components/movie-similar";
 import { IParms } from "../layout";
+import Loading from "../../../../../components/Loading";
 
 export default function Similar({params:{id}}:IParms){
   return(
-    <MovieSimilar id={id} />
+    <Suspense fallback={<Loading />}>
+      <MovieSimilar id={id} />
+    </Suspense>
   );
 }
