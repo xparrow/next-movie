26da530efacfc95a5519c77541b7d5fabb5ddf13
darkwhat@naver.com <darkwhat@naver.com>
2024-02-16 15:22:09 +0900

    ee

--- components/detail-nav.tsx
+++ components/detail-nav.tsx
@@ -1,25 +1,25 @@
 'use client';
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+
 import styles from "../styles/navigation.module.css";
 
 export default function DetailNav({id}){
-	const path = usePathname();
+
 	return (
 		<nav className={styles.detailNav}>
 			<ul>
 				<li>
-					<Link href={`/movies/${id}`}>Videos {path === `/movies/${id}` ? "✅" : ""}</Link> 
+					<Link href={`/movies/${id}`}>Videos</Link> 
 				</li>
 				<li>
-					<Link href={`/movies/${id}/credits`}>Credits {path === `/movies/${id}/credits` ? "✅" : ""}</Link>
+					<Link href={`/movies/${id}/credits`}>Credits</Link>
 				</li>
 				<li>
-					<Link href={`/movies/${id}/providers`}>Providers {path === `/movies/${id}/providers` ? "✅" : ""}</Link>
+					<Link href={`/movies/${id}/providers`}>Providers</Link>
 				</li>
 				<li>
-					<Link href={`/movies/${id}/similar`}>Similar {path === `/movies/${id}/similar` ? "✅" : ""}</Link>
+					<Link href={`/movies/${id}/similar`}>Similar</Link>
 				</li>
 			</ul>
 		</nav>
